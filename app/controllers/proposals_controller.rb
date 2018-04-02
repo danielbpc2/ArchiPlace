4ca6e9ed79e27653623622683a4f40e3cf9231b8
Danielle Alvino <daniellealvino@hotmail.com>
2018-04-02 19:00:34 -0300

    implemented index, new and create to proposal controller

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,14 +1,24 @@
 class ProposalsController < ApplicationController
+  before_action :set_proposal, only: [:show, :edit]
+
   def index
+    @proposals = Proposal.all
   end
 
   def show
   end
 
   def new
+    @proposal = Proposal.new
   end
 
   def create
+    @proposal = Proposal.new
+      if @proposal.save
+        redirect_to proposal_path, notice: 'Your proposal has been created!'
+      else
+        render :new
+    end
   end
 
   def edit
@@ -17,3 +27,9 @@ class ProposalsController < ApplicationController
   def update
   end
 end
+
+private
+
+  def set_proposal
+    @proposal = Proposal.find(params[:id])
+  end
