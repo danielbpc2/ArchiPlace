c97a5fe24b603e9f9940070dd560c16c56d649d9
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-02 17:51:36 -0300

    seed and uploader adjust

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'seeding'
+projectseed = Project.new
+projectseed.title = "My project"
+projectseed.location = "São Paulo"
+projectseed.dimensions = "4mx3mx4mx3m"
+projectseed.description = "This is my description, this is a test"
+projectseed.budget = "R$100,00"
+projectseed.deadline = "30 dias"
+projectseed.status = "Aberto"
+projectseed.room = "Quarto"
+projectseed.user = User.last
+projectseed.save
+puts 'saved'
