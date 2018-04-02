9ffb780fc98b4b736b564bcf5681b63b88b86252
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-02 16:56:17 -0300

    Project controller and views created with rails generate, deleted useless pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  # root is a home page controlled by PagesController
   root to: 'pages#home'
+
   # Routes from project
   resource :projects
+
   # routes from proposals
   resource :proposals, except: :destroy
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
