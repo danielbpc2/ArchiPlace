0622fa684ba684bf55c0de50f1b7f1f1fb5f1d2d
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-03 14:36:58 -0300

    validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,20 @@ class Project < ApplicationRecord
   mount_uploader :image, PhotoUploader
   belongs_to :user
   has_many :proposals
+  #Validations
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :dimensions, presence: true
+  validates :room, presence: true
+  validates :room, inclusion: { in:
+    [
+      'Bedroom', 'Kitchen', 'Bathroom', 'laundry room',
+      'Attic', 'Dressing room', 'Garage', 'Lounge',
+      'Dining room', 'Cellar', 'Basement','Boiler room','Living room'
+    ]
+  }
+  validates :budget, presence: true
+  validates :budget, numericality: { only_integer: true }
+  validates :deadline, presence: true
 end
