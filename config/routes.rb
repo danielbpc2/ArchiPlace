0a2a22b62b06b5e0bdce8e659020bed5b8b08aed
Daniel Carvalho <danielbpc2@hotmail.com>
2018-04-03 11:40:50 -0300

    fixed routes, fixed project controller, fixed views, created some stuff on views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  devise_for :users
   # root is a home page controlled by PagesController
+  root to: 'pages#home'
+  #devise routes
+  devise_for :users
+  #if needed routes with :user_id params
   resources :user, only: [] do
-  # Routes from project nested in user
-    resources :projects, only: [:show, :edit, :update]
-  # routes from proposals nested in user
   end
 
   # Routes from project
-  resources :projects, only: [:index, :new, :create, :destroy] do
+  resources :projects, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
     resources :proposals, only: [:show, :new, :create, :edit, :update]
   end
   # routes from proposals
