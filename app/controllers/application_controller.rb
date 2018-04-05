4bcc009caa937f7b3f1e35306b1ea3ece7598922
Danielle Alvino <daniellealvino@hotmail.com>
2018-04-05 14:50:19 -0300

    cant create project being a owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_back(fallback_location: root_path, alert: "(Error 403): Your account is not able to do this action.")
