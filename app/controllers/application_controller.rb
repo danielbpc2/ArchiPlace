aade7402f91ad76a5ab0c2b2f15036d01bf34cc0
Daniel Topper <daniel.topper@gmail.com>
2018-04-05 12:15:15 -0300

    correction app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :birthday, :role, :image, :image_cache])
   end
 
-  # rescue_from CanCan::AccessDenied do |exception|
-     # redirect_back(fallback_location: root_path, alert: "(Error 403): Your account is not able to do this action.")
-  # end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_back(fallback_location: root_path, alert: "(Error 403): Your account is not able to do this action.")
+  end
 end
